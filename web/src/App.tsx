d1c8a8cd4d7bbff0e18a61aedb4feaa4fb20017b
Berk Cambaz <berkcambaz12321@gmail.com>
2023-06-16 19:26:48 +0300

    Remove top level awaits for compability

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,16 @@
+import { blockchain, IBlockchain } from "@core/blockchain";
+import { transaction } from "@core/transaction";
+import { wallet } from "@core/wallet";
 import { MantineProvider } from "@mantine/core";
 import { useEffect } from "react";
 import { Outlet, ScrollRestoration, useLocation } from "react-router-dom"
+import { IAppWallet } from "./components/types/wallet";
 import { useAppStore } from "./stores/appStore";
+import { useCryptoStore } from "./stores/cryptoStore";
 import { theme } from "./styles/theme";
 
 export default function App() {
   const location = useLocation();
-
   useEffect(() => {
     if (location.pathname.indexOf("/blockchain") !== -1) useAppStore.setState(s => { s.route = "blockchain" });
     else if (location.pathname.indexOf("/transactions") !== -1) useAppStore.setState(s => { s.route = "transactions" });
@@ -14,6 +18,42 @@ export default function App() {
     else if (location.pathname.indexOf("/settings") !== -1) useAppStore.setState(s => { s.route = "settings" });
     else useAppStore.setState(s => { s.route = "any" });
   }, [location.pathname]);
+
+  const ready = useCryptoStore(state => state.ready);
+  useEffect(() => {
+    if (ready) return;
+
+    (async () => {
+      const _blockchain: IBlockchain = blockchain.create(2, 100);
+
+      const miner = wallet.create();
+      const man = wallet.create();
+      const woman = wallet.create();
+      const _wallets: IAppWallet[] = [
+        { name: "Miner", keys: miner },
+        { name: "Man", keys: man },
+        { name: "Woman", keys: woman },
+      ];
+
+      await blockchain.minePendingTransactions(_blockchain, miner.public);
+
+      const tx1 = transaction.create(miner.public, man.public, 25);
+      transaction.sign(tx1, miner.private);
+      blockchain.addTransaction(_blockchain, tx1);
+
+      const tx2 = transaction.create(miner.public, woman.public, 25);
+      transaction.sign(tx2, miner.private);
+      blockchain.addTransaction(_blockchain, tx2);
+
+      await blockchain.minePendingTransactions(_blockchain, miner.public);
+
+      useCryptoStore.setState(s => {
+        s.blockchain = _blockchain;
+        s.wallets = _wallets;
+        s.ready = true;
+      });
+    })();
+  }, [ready]);
 
   return (
     <>
--- web/src/stores/cryptoStore.ts
+++ web/src/stores/cryptoStore.ts
@@ -1,38 +1,13 @@
 import { IAppWallet } from "@/components/types/wallet";
 import { blockchain, IBlockchain } from "@core/blockchain";
-import { transaction } from "@core/transaction";
-import { wallet } from "@core/wallet";
-
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { persist } from "zustand/middleware";
 
-const _blockchain: IBlockchain = blockchain.create(2, 100);
-
-const miner = wallet.create();
-const man = wallet.create();
-const woman = wallet.create();
-const _wallets: IAppWallet[] = [
-  { name: "Miner", keys: miner },
-  { name: "Man", keys: man },
-  { name: "Woman", keys: woman },
-];
-
-await blockchain.minePendingTransactions(_blockchain, miner.public);
-
-const tx1 = transaction.create(miner.public, man.public, 25);
-transaction.sign(tx1, miner.private);
-blockchain.addTransaction(_blockchain, tx1);
-
-const tx2 = transaction.create(miner.public, woman.public, 25);
-transaction.sign(tx2, miner.private);
-blockchain.addTransaction(_blockchain, tx2);
-
-await blockchain.minePendingTransactions(_blockchain, miner.public);
-
 export interface CryptoStoreState {
   blockchain: IBlockchain;
   wallets: IAppWallet[];
+  ready: boolean;
 }
 
 export interface CryptoStoreAction {
@@ -40,8 +15,9 @@ export interface CryptoStoreAction {
 }
 
 const initialState: CryptoStoreState = {
-  blockchain: _blockchain,
-  wallets: _wallets,
+  blockchain: blockchain.create(2, 100),
+  wallets: [],
+  ready: false,
 };
 
 export const useCryptoStore = create(
